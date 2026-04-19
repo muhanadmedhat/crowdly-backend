e3226dececf89c3949827b803d859818ccdf9617
muhanadmedhat <muhanadronaldo@yahoo.com>
2026-04-19 02:14:43 +0200

    Edited cloudinary

--- projects/models/category_model.py
+++ projects/models/category_model.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 class Category(models.Model):
     name        = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-    image = CloudinaryField('image', folder='category',  blank=True, null=True)
+    image = CloudinaryField('image', folder='crowdly/category',  blank=True, null=True)
     created_by  = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
--- projects/models/projectImage_model.py
+++ projects/models/projectImage_model.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 
 class ProjectImage(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
-    image = CloudinaryField('image', folder='projects',  blank=True, null=True)
+    image = CloudinaryField('image', folder='crowdly/projects',  blank=True, null=True)
     order   = models.IntegerField(default=0)
 
     def __str__(self):
